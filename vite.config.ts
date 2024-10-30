b06fdab76a29f23ce9489a9316b9d27bfbc743f3
folka2134 <folka2134@gmail.com>
2024-10-30 09:32:05 +0000

    setup electron and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set base path of application for build
+  base: "./",
   // Change build directory, to not conflict with electron builder
   build: {
     outDir: "dist-react",
